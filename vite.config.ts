f78b624b91785c9ff08cada24188d0d22e267292
acaldas <acaldas@powerhouse.inc>
2023-05-05 17:50:50 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig(({ mode }) => ({
     plugins: [
-        // nodePolyfills(),
         react({ jsxRuntime: 'classic' }),
-        // tsConfigPaths({ root: './' }),
-        // dts({
-        //     include: ['src'],
-        // }),
+        dts({
+            include: ['src'],
+        }),
     ],
     build: {
         sourcemap: false,
         rollupOptions: {
-            external: [
-                'react',
-                'react/jsx-runtime',
-                // '@graphql-codegen/core',
-                // '@graphql-codegen/typescript',
-                // 'graphql-codegen-typescript-validation-schema',
-            ],
-            // plugins: [resolvePlugin(), commonjs(), esbuild(), css()],
+            external: ['react', 'react/jsx-runtime'],
         },
         commonjsOptions: {
             include: [],
-            // ignore: ['module'],
-            // transformMixedEsModules: true,
-            // esmExternals: true,
         },
         lib: {
             entry: resolve('src', 'index.ts'),
@@ -44,38 +33,4 @@ export default defineConfig(({ mode }) => ({
     define: {
         __vite_process_env_NODE_ENV: JSON.stringify(mode),
     },
-    // optimizeDeps: {
-    //     include: [
-    //         // 'monaco-editor',
-    //         'lodash',
-    //         '@graphql-codegen/plugin-helpers',
-    //         'graphql-codegen-typescript-validation-schema',
-    //         '@graphql-tools/relay-operation-optimizer > @ardatan/relay-compiler',
-    //     ],
-    //     esbuildOptions: {
-    //         plugins: [
-    //             NodeGlobalsPolyfillPlugin({
-    //                 process: true,
-    //                 buffer: true,
-    //             }),
-    //             NodeModulesPolyfillPlugin(),
-    //         ],
-    //     },
-    //     // exclude: [
-    //     //     ...Object.keys(packageJson.peerDependencies),
-    //     //     // '@graphql-codegen/core',
-    //     //     // '@graphql-codegen/typescript',
-    //     //     // 'graphql-codegen-typescript-validation-schema',
-    //     // ],
-    // },
-    // resolve: {
-    //     alias: {
-    //         './runtimeConfig': './runtimeConfig.browser',
-    //         // crypto: 'rollup-plugin-node-polyfills/polyfills/crypto-browserify',
-    //         path: 'rollup-plugin-node-polyfills/polyfills/path',
-    //         util: 'rollup-plugin-node-polyfills/polyfills/util',
-    //         process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
-    //         buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
-    //     },
-    // },
 }));
