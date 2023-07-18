08719d185b7dac82151e4a345efc76b9e3b57830
acaldas <acaldas@powerhouse.inc>
2023-07-18 16:36:48 +0100

    Added support for setting initial document state

--- src/document-model/reducer.ts
+++ src/document-model/reducer.ts
@@ -1,14 +1,13 @@
 import {
     createEmptyExtendedDocumentModelState,
+    ExtendedDocumentModelState,
     reducer,
 } from '@acaldas/document-model-libs/browser/document-model';
 
 import { useDocumentReducer } from '../common';
 
-export default function useDocumentModelReducer() {
-    return useDocumentReducer(
-        reducer,
-        createEmptyExtendedDocumentModelState(),
-        createEmptyExtendedDocumentModelState
-    );
+export default function useDocumentModelReducer(
+    initialState: ExtendedDocumentModelState = createEmptyExtendedDocumentModelState()
+) {
+    return useDocumentReducer(reducer, initialState);
 }
