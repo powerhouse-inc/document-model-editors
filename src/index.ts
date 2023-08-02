84608352ad974103db22a018b8a463537afd8d64
acaldas <acaldas@powerhouse.inc>
2023-08-02 12:32:19 +0100

    Removed Document editor from bundle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * as BudgetStatement from './budget-statement';
-export * as Document from './document';
 export * as DocumentModel from './document-model';
 export * as ScopeFramework from './scope-framework';
